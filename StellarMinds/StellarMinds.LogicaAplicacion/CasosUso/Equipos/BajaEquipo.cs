9c184983e9d1fd389cc7f0ecdfa885f03d8ac657
favabb <buriolfavb@gmail.com>
2026-04-22 22:28:23 -0300

    Se agregan CU para Equipo: Baja, Update y Delete

--- StellarMinds/StellarMinds.LogicaAplicacion/CasosUso/Equipos/BajaEquipo.cs
+++ StellarMinds/StellarMinds.LogicaAplicacion/CasosUso/Equipos/BajaEquipo.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using StellarMinds.Infraestructura.InterfacesRepositorio.Equipos;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace StellarMinds.LogicaAplicacion.CasosUso.Equipos
 {
-    internal class BajaEquipo
+    public class BajaEquipo
     {
+        private IRepositorioEquipo _repo;
+
+
+        public BajaEquipo (IRepositorioEquipo repo)
+        {
+            _repo = repo;
+        }
+        public void Execute(int id)
+        {
+            _repo.Delete(id);
+        }
     }
 }
--- StellarMinds/StellarMinds.LogicaAplicacion/CasosUso/Equipos/EditarEquipo.cs
+++ StellarMinds/StellarMinds.LogicaAplicacion/CasosUso/Equipos/EditarEquipo.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using StellarMinds.Infraestructura.InterfacesRepositorio.Equipos;
+using StellarMinds.LogicaAplicacion.Dtos.Equipos.StellarMinds.LogicaAplicacion.Dtos.Equipos;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace StellarMinds.LogicaAplicacion.CasosUso.Equipos
 {
-    internal class EditarEquipo
+    public class EditarEquipo
     {
+        private IRepositorioEquipo _repo;
+
+        public EditarEquipo(IRepositorioEquipo repo)
+        {
+            _repo = repo;
+        }
+
+
+        public void Execute(int id, AltaEquipoDto Obj)
+        {
+            //_repo.Update(id, AutorMapper.FromDto(Obj));
+        }
     }
 }
