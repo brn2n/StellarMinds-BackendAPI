6738a235d5360264962d02811b1c8d51b2819b28
FERAR <FERAR@DESKTOP-DN58GBV>
2026-04-22 22:17:20 -0300

    "Se creo dto Equipo y view"

---
+++ StellarMinds/StellarMinds.LogicaAplicacion/Dtos/Equipos/AltaEquipoDto.cs
@@ -0,0 +1,26 @@
+﻿
+namespace StellarMinds.LogicaAplicacion.Dtos.Equipos
+{
+    namespace StellarMinds.LogicaAplicacion.Dtos.Equipos
+    {
+        public record AltaEquipoDto
+        {
+           public int Id { get; init; }
+            public string TipoEquipo { get; set; }
+            public string Marca { get; set; } 
+            public string Modelo { get; set; } 
+            public int CantDisponible { get; set; }
+            public double? Apertura { get; set; }
+            public string? RelacionFocal { get; set; }
+            public double? DistanciaFocal { get; set; }
+            public double? Peso { get; set; }
+            public string? TipoMontura { get; set; }
+            public double? CargaUtilSoportada { get; set; }
+            public string? TipoSensor { get; set; }
+            public double? ResolucionMP { get; set; }
+            public bool? EsColor { get; set; }
+            public double? DistanciaFocalOcular { get; set; }
+            public double? CampoVision { get; set; }
+        }
+    }
+}
