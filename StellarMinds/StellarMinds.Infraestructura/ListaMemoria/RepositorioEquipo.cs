dcec4964b34142f9b8b5d454d364c5d0cb71b1bc
favabb <buriolfavb@gmail.com>
2026-04-22 22:12:49 -0300

    Se agrega RepositorioEquipo

--- StellarMinds/StellarMinds.Infraestructura/ListaMemoria/RepositorioEquipo.cs
+++ StellarMinds/StellarMinds.Infraestructura/ListaMemoria/RepositorioEquipo.cs
@@ -1,4 +1,4 @@
-﻿using StellarMinds.Infraestructura.InterfacesRepositorio;
+﻿using StellarMinds.Infraestructura.InterfacesRepositorio.Equipos;
 using StellarMinds.LogicaNegocio.Entidades.Equipos;
 using System;
 using System.Collections.Generic;
@@ -15,9 +15,24 @@ namespace StellarMinds.Infraestructura.ListaMemoria
                 _equipos.Add(equipo);
             }
 
+        public void Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
         public IEnumerable<Equipo> GetAll()
         {
             return _equipos;
+        }
+
+        public Equipo GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Equipo obj)
+        {
+            throw new NotImplementedException();
         }
     }
 }
