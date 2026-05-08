930d32299d5cc8a4b1aad2632934c3704c1f8212
Fer Arriondo <ferarriondo@gmail.com>
2026-05-07 21:16:33 -0300

    "Aaa"

--- StellarMinds/StellarMinds.LogicaNegocio/Entidades/ObjetosCelestes/ObjetoCeleste.cs
+++ StellarMinds/StellarMinds.LogicaNegocio/Entidades/ObjetosCelestes/ObjetoCeleste.cs
@@ -2,7 +2,6 @@
 {
     public class ObjetoCeleste
     {
-        public int Id { get; set; }
         public string Nombre { get; set; }
         public string Tipo { get; set; }
         public int Magnitud { get; set; }
